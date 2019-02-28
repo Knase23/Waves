36fee06aa1049f6064f84346562e76b30f5499ee
Jesper <Jespe.uddefors.95@gmail.com>
2019-03-01 00:28:13 +0100

    Initial Unity Commit

---
+++ Assets/Scripts/Test/ExpandingCircle.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExpandingCircle : MonoBehaviour
+{
+    private GameObject circleCenter;
+    public int degreesPerSegment = 10;
+    [Range(0f,20f)]public float distanceFromCenter = 5f;
+    private float previousDistanceFromCenter = 0;
+    public bool centerHaveLineRenderer = false;
+    public Material materialForNodes;
+    private List<NodeScript> listOfNodes = new List<NodeScript>();
+    // Start is called before the first frame update
+    void Start()
+    {
+        circleCenter = new GameObject("Circle Center");
+        circleCenter.transform.parent = transform;
+        circleCenter.transform.localPosition = Vector3.zero;
+        GameObject node;
+        float curAngle = 0;
+        NodeScript firstNode = null;
+        NodeScript previousNode = null;
+        
+
+        LineRenderer lineRenderer = null;
+        if(centerHaveLineRenderer)
+            lineRenderer = circleCenter.AddComponent<LineRenderer>();
+
+        if (lineRenderer)
+        {
+            lineRenderer.startWidth = 0.2f;
+            lineRenderer.endWidth = 0.2f;
+            lineRenderer.positionCount = 360 / degreesPerSegment + 1;
+        }
+
+        for (int i = 0, j = 0; i < 360/ degreesPerSegment + 1; i++, j++)
+        {
+            node = new GameObject("Node: " + i);
+            node.transform.parent = circleCenter.transform;
+            node.transform.localPosition = new Vector3(Mathf.Cos(Mathf.Deg2Rad *curAngle) * distanceFromCenter, 0, Mathf.Sin(Mathf.Deg2Rad * curAngle) * distanceFromCenter);
+
+            
+
+            curAngle += degreesPerSegment;
+            NodeScript  nS = node.AddComponent<NodeScript>();
+            nS.materialForNodes = materialForNodes;
+            nS.center = circleCenter;
+            nS.directionFromCenter = new Vector3(node.transform.localPosition.x, node.transform.localPosition.y, node.transform.localPosition.z);
+            nS.directionFromCenter.Normalize();
+            if (previousNode != null)
+            {
+                nS.left = previousNode;
+                previousNode.right = nS;
+            }
+            if (firstNode == null)
+            {
+                firstNode = nS;
+            }
+            previousNode = nS;
+            listOfNodes.Add(nS);
+
+            if (lineRenderer)
+                lineRenderer.SetPosition(j, previousNode.transform.position);
+            
+        }
+
+        if (lineRenderer)
+            lineRenderer.loop = true;
+
+        previousNode.right = firstNode;
+        firstNode.left = previousNode;
+
+        foreach (NodeScript item in listOfNodes)
+        {
+            item.CreateLineBetweenNodes();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (previousDistanceFromCenter != distanceFromCenter)
+        {
+            foreach (var item in listOfNodes)
+            {
+                item.transform.localPosition = item.directionFromCenter * distanceFromCenter;
+                
+            }
+            foreach (var item in listOfNodes)
+            {
+                item.UpdateMesh();
+            }
+            previousDistanceFromCenter = distanceFromCenter;
+        }
+    }
+}
---
+++ Assets/Scripts/Test/NodeScript.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NodeScript : MonoBehaviour
+{
+    public GameObject center;
+    public NodeScript left;
+    public NodeScript right;
+    private LineRenderer lineRenderer;
+    private Collider collider;
+    internal Vector3 directionFromCenter;
+    public Material materialForNodes;
+    MeshFilter filter;
+    List<Vector3> vertices = new List<Vector3>();
+
+    public void CreateLineBetweenNodes()
+    {
+        lineRenderer = gameObject.AddComponent<LineRenderer>();
+        lineRenderer.material = materialForNodes;
+        lineRenderer.SetPosition(0, transform.position);
+        lineRenderer.SetPosition(1, right.transform.position);
+        lineRenderer.startWidth = 0.1f;
+        lineRenderer.endWidth = 0.1f;
+        lineRenderer.material.color = Color.green;
+        CreateMesh();
+    }
+    public void CreateColliderLineBetweenNodes()
+    {
+       // MeshCollider s = new MeshCollider();
+        //s.
+    }
+    public void CreateMesh()
+    {
+        Mesh mesh = new Mesh();
+        vertices.Add(Vector3.zero);
+        vertices.Add((right.transform.position - transform.position));
+        vertices.Add(Vector3.up * 0.2f);
+        vertices.Add((right.transform.position - transform.position) + Vector3.up * 0.2f);
+
+        mesh.SetVertices(vertices);
+        mesh.triangles = new int[]
+        {
+            1, 0, 2,  // Front Down
+            3, 1, 2, // Front Up
+            2, 0, 1,  //Back Down
+            2, 1, 3, // Back Up
+        };
+        mesh.RecalculateBounds();
+
+        filter = gameObject.AddComponent<MeshFilter>();
+        filter.mesh = mesh;
+        gameObject.AddComponent<MeshRenderer>();
+        gameObject.AddComponent<MeshCollider>();
+    }
+
+    private void Update()
+    {
+        if (lineRenderer)
+        {
+            UpdateLine();
+        }
+
+        Ray ray = new Ray(transform.position + (Vector3.up*0.1f), right.transform.position - transform.position);
+        foreach (var item in Physics.RaycastAll(ray, (right.transform.position - transform.position).magnitude))
+        {
+            if(item.transform != transform && item.transform.parent != transform.parent)
+            {
+                Debug.Log( transform.name +" is Hitting: " + item.transform.name);
+                lineRenderer.material.color = Color.red;
+                break;
+            }
+            else
+            {
+                lineRenderer.material.color = Color.green;
+            }
+        }
+        
+    }
+    void UpdateLine()
+    {
+        lineRenderer.SetPosition(0, transform.position);
+        lineRenderer.SetPosition(1, right.transform.position);
+    }
+    public void UpdateMesh()
+    {
+        vertices[1] = right.transform.position - transform.position;
+        vertices[3] = (right.transform.position - transform.position) + Vector3.up * 0.2f;
+        filter.mesh.SetVertices(vertices);
+    }
+}
