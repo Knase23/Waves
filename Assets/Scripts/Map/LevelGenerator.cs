c485647b55bb2500fc7c0e1a6fe5b6ab391846e9
Jesper Uddefors <jesper.uddefors.95@gmail.com>
2019-09-30 15:45:16 +0200

    Planning of Level generation!

--- Assets/Scripts/Map/LevelGenerator.cs
+++ Assets/Scripts/Map/LevelGenerator.cs
@@ -39,212 +39,45 @@ public class LevelGenerator : MonoBehaviour
     [Header("Tolerance")]
     public int maxNumberOfFailedPlacements = 10;
 
-    //Plans/Ideas/Questions
-    /*  Astroids should spilt when enough damage is done.
-     *  They will have a small velocity when they get hit.
-     *  The smaller they are, the more damage.
-     *  You can only be damaged by them when you or it have a greater impact velocity.
-     *  // Problems that might be later:
-     *  - If it goes online multiplayer, how should we sync all astorids states to all clients
-     */
-
     /// <summary>
     /// Generates a map of astiods with the given prefabs
     /// </summary>
     public void GenerateLevel()
     {
-        ClearLevel();
-        float[,] noiseMap;
+        // Use Poisson-disc distribution method do spawn in a level.
 
-        if (randomOffset)
-        {
-            noiseMap = GenerateNoiseMap(mapSize.x, mapSize.y, noiseZoom, UnityEngine.Random.Range(-100, 100), UnityEngine.Random.Range(-100, 100));
-        }
-        else
-        {
-            noiseMap = GenerateNoiseMap(mapSize.x, mapSize.y, noiseZoom, perlinOffset.x, perlinOffset.y);
+        // All astriod can have a minimum distance from the borders by 1 unit
+        // Asteriods have a set Radius, determined by their prefabs Asteriod scripts placement radius
 
-        }
-        Vector3 centerPositionCorrection = new Vector3(transform.position.x - (mapSize.x / 2), transform.position.y, transform.position.z - (mapSize.y / 2));
+        //Asteriods placement order
 
-        for (int x = 0; x < mapSize.x; x++)
-        {
-            for (int y = 0; y < mapSize.y; y++)
-            {
-                // Small
-                if (noiseMap[x, y] < smallRange.max && noiseMap[x, y] > smallRange.min)
-                {
-                    Vector3 position = new Vector3(centerPositionCorrection.x + x, centerPositionCorrection.y, centerPositionCorrection.z + y) + new Vector3(UnityEngine.Random.Range(-1, 1), 0, UnityEngine.Random.Range(-1, 1));
-                    if (EditorApplication.isPlaying)
-                    {
-                        Instantiate(smallAstroidPrefabs, position, UnityEngine.Random.rotation, smallTransform);
-                    }
-                    else
-                    {
-                        GameObject prefab = PrefabUtility.InstantiatePrefab(smallAstroidPrefabs, smallTransform) as GameObject;
-                        prefab.transform.position = position;
-                        prefab.transform.rotation = UnityEngine.Random.rotation;
-                    }
-                }
+        // Large 
+        /*
+         * They have a set Radius, determined by the prefabs Asteriod scripts placement radius
+         * There minimum distance from a Another Large Asteriod can be there Placement Radius + Tolerence 
+         */
 
-                // Medium
-                if (noiseMap[x, y] < mediumRange.max && noiseMap[x, y] > mediumRange.min)
-                {
-                    Vector3 position = new Vector3(centerPositionCorrection.x + x, centerPositionCorrection.y, centerPositionCorrection.z + y) + new Vector3(UnityEngine.Random.Range(-1, 1), 0, UnityEngine.Random.Range(-1, 1));
+        // Medium
+        /*
+        * There minimum distance from a Large Asteriod can be there Placement Radius + LargeTolerence 
+        * There minimum distance from a Medium Asteriod can be there Placement Radius + MediumTolerence
+        */
+        // Small
+        /*
+         * The Small must have atleast 1 Medium or Large asteriod near it
+         * 
+         * There minimum distance from a Large Asteriod can be there Placement Radius + LargeMinTolerence 
+         * There maximum distance from a Large Asteriod can be there Placement Radius + LargeMaxTolerence 
+         * 
+         * There minimum distance from a Medium Asteriod can be there Placement Radius + MediumMinTolerence
+         * There maximum distance from a Medium Asteriod can be there Placement Radius + MediumMaxTolerence
+         * 
+         * There minimum distance from a Small Asteriod can be there Placement Radius + Tolerence 
+         */
 
-                    if (EditorApplication.isPlaying)
-                    {
-                        Instantiate(mediumAstroidPrefabs, position , UnityEngine.Random.rotation, mediumTransform);
-                    }
-                    else
-                    {
+         // All of them will be added in a list, that is then sent out to all clients, so they can replicate the scene!
 
-                        GameObject prefab = PrefabUtility.InstantiatePrefab(mediumAstroidPrefabs, mediumTransform) as GameObject;
-                        prefab.transform.position = position;
-                        prefab.transform.rotation = UnityEngine.Random.rotation;
-                    }
-                }
-                //Large
-                if (noiseMap[x, y] < largeRange.max && noiseMap[x, y] > largeRange.min)
-                {
-                    Vector3 position = new Vector3(centerPositionCorrection.x + x, centerPositionCorrection.y, centerPositionCorrection.z + y) + new Vector3(UnityEngine.Random.Range(-1, 1), 0, UnityEngine.Random.Range(-1, 1));
-
-                    if (EditorApplication.isPlaying)
-                    {
-                        Instantiate(largeAstroidPrefabs, position, UnityEngine.Random.rotation, largeTransform);
-                    }
-                    else
-                    {
-                        GameObject prefab = PrefabUtility.InstantiatePrefab(largeAstroidPrefabs, largeTransform) as GameObject;
-                        prefab.transform.position = position;
-                        prefab.transform.rotation = UnityEngine.Random.rotation;
-
-                    }
-                }
-            }
-        }
-
-        CheckAndRemoveObjectsThatIsInsideEachOther(largeTransform);
-        CheckAndRemoveObjectsThatIsInsideEachOther(mediumTransform);
-        CheckAndRemoveObjectsThatIsInsideEachOther(smallTransform);
-
-        List<GameObject> medium = GetAstriodsFromTransform(mediumTransform);
-        List<GameObject> mediumToRemove = GetListOfIntercetionsBetweenToListsOfObjects(GetAstriodsFromTransform(largeTransform),medium);
-        foreach (var item in mediumToRemove)
-        {
-            DestroyImmediate(item);
-        }
-
-        List<GameObject> smallToRemove = GetListOfIntercetionsBetweenToListsOfObjects(medium,GetAstriodsFromTransform(smallTransform));
-        foreach (var item in smallToRemove)
-        {
-            DestroyImmediate(item);
-        }
     }
-
-
-    private List<GameObject>  GetListOfIntercetionsBetweenToListsOfObjects(List<GameObject> list, List<GameObject> list2)
-    {
-        List<GameObject> smallToRemove = new List<GameObject>();
-        for (int i = 0; i < list.Count; i++)
-        {
-            Bounds mediumBounds = GetGameObjectsRealBounds(list[i]);
-
-            // Want a better way of checking this!
-            for (int k = 0; k < list2.Count; k++)
-            {
-                if (smallToRemove.Contains(list2[k]))
-                {
-                    continue;
-                }
-                Bounds smallBounds = GetGameObjectsRealBounds(list2[k]);
-                if (mediumBounds.Intersects(smallBounds))
-                {
-                    //Debug.Log(i +" added "+  k + " Is in the RemoveList");
-                    smallToRemove.Add(list2[k]);
-                    continue;
-                }
-            }
-        }
-        return smallToRemove;
-    }
-
-    /// <summary>
-    /// Help function for determining bounds in Not PlayMode!
-    /// </summary>
-    /// <param name="gameObject"></param>
-    /// <returns></returns>
-    private Bounds GetGameObjectsRealBounds(GameObject gameObject)
-    {
-       return new Bounds(gameObject.transform.position, gameObject.GetComponentInChildren<Collider>().bounds.extents * 1.5f);
-    }
-    private void CheckAndRemoveObjectsThatIsInsideEachOther(Transform transform)
-    {
-        List<GameObject> astriodsToRemove = CheckListOfGameObjectsIfTheyIntercets(GetAstriodsFromTransform(transform));
-        foreach (var item in astriodsToRemove)
-        {
-            DestroyImmediate(item);
-        }
-    }
-
-    private List<GameObject> CheckListOfGameObjectsIfTheyIntercets(List<GameObject> list)
-    {
-        List<GameObject> astriodsToRemove = new List<GameObject>();
-        for (int i = 0; i < list.Count; i++)
-        {
-            if (astriodsToRemove.Contains(list[i]))
-            {
-                continue;
-            }
-
-            Bounds astriodBounds = GetGameObjectsRealBounds(list[i]);
-
-            // Want a better way of checking this!
-            for (int k = i + 1; k < list.Count; k++)
-            {
-                Bounds realBounds = GetGameObjectsRealBounds(list[k]);
-
-                if (astriodBounds.Intersects(realBounds))
-                {
-                    //Debug.Log(i +" added "+  k + " Is in the RemoveList");
-                    astriodsToRemove.Add(list[k]);
-                }
-
-            }
-
-        }
-        return astriodsToRemove;
-    }
-    /// <summary>
-    /// Returns a Noise Map that can be used to make a level
-    /// </summary>
-    /// <param name="mapDepth"></param>
-    /// <param name="mapWidth"></param>
-    /// <param name="scale"> Acts as a Zoom on the noise map</param>
-    /// <returns>Noise Map</returns>
-    public float[,] GenerateNoiseMap(int mapDepth, int mapWidth, float scale, float offsetX = 0, float offsetZ = 0)
-    {
-        // create an empty noise map with the mapDepth and mapWidth coordinates
-        float[,] noiseMap = new float[mapDepth, mapWidth];
-
-        for (int zIndex = 0; zIndex < mapDepth; zIndex++)
-        {
-            for (int xIndex = 0; xIndex < mapWidth; xIndex++)
-            {
-                // calculate sample indices based on the coordinates and the scale
-                float sampleX = (xIndex / scale) + offsetX;
-                float sampleZ = (zIndex / scale) + offsetZ;
-
-                // generate noise value using PerlinNoise
-                float noise = Mathf.PerlinNoise(sampleX, sampleZ);
-
-                noiseMap[zIndex, xIndex] = noise;
-            }
-        }
-        return noiseMap;
-    }
-
-
     /// <summary>
     /// Clears all astriods inside the Astriod list. AKA makes the map empty
     /// </summary>
@@ -264,9 +97,9 @@ public class LevelGenerator : MonoBehaviour
     /// <param name="tag"></param>
     /// <param name="transform"></param>
     /// <param name="gameObjects"></param>
-    private void AddGameObjectWithTagFormTransform(string tag,Transform transform, ref List<GameObject> gameObjects)
+    private void AddGameObjectWithTagFormTransform(string tag, Transform transform, ref List<GameObject> gameObjects)
     {
-        if(transform.tag == tag)
+        if (transform.tag == tag)
         {
             gameObjects.Add(transform.gameObject);
 
