359a2dfdc5922e21b2bd6ed28989fbde0708aca2
Jesper Uddefors - Home Computer <Jesper.uddefors.95@gmail.com>
2019-10-05 23:21:21 +0200

    SeperatedSamples to there own files

---
+++ Assets/Scripts/Asteriod/LargeAsteriodSample.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Large Asteriod Placement Rules", menuName = "Placement Rules/Large Asteriod", order = 1)]
+public class LargeAsteriodSample : Sample
+{
+    // Large 
+    /*
+     * They have a set Radius, determined by the prefabs Asteriod scripts placement radius
+     * There minimum distance from a Another Large Asteriod can be there Placement Radius + Tolerence 
+     */
+    public float minimumDistanceFromOtherLargeAsteriod = 1;
+
+    public override bool Validation(Vector3 position, Collider collider)
+    {
+        Asteriod other = collider.GetComponent<Asteriod>();
+        //Is it the minimum Distance from each other
+        if (SpehereIntersecting(position, other, minimumDistanceFromOtherLargeAsteriod))
+        {
+            return false;
+        }
+
+        return true;
+    }
+}
---
+++ Assets/Scripts/Asteriod/MediumAsteriodSample.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Medium Asteriod Placement Rules", menuName = "Placement Rules/Medium Asteriod", order = 2)]
+public class MediumAsteriodSample : Sample
+{
+    // Medium
+    /*
+    * There minimum distance from a Large Asteriod can be there Placement Radius + LargeTolerence 
+    * There minimum distance from a Medium Asteriod can be there Placement Radius + MediumTolerence
+    */
+    public float largeAsteriodThreshold = 2;
+    public float mediumAsteriodThreshold = 4;
+
+    public override bool Validation(Vector3 position, Collider collider)
+    {
+        //
+        Asteriod other = collider.GetComponent<Asteriod>();
+        if (SpehereIntersecting(position, other, 1))
+        {
+            return false;
+        }
+        if (other is LargeAsteriod)
+        {
+            if (SpehereIntersecting(position, other, largeAsteriodThreshold))
+            {
+                return false;
+            }
+        }
+        if (other is MediumAsteriod)
+        {
+            if (SpehereIntersecting(position, other, mediumAsteriodThreshold))
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+}
+
